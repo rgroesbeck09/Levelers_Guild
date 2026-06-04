283d5c133212907a0c79fe81ff9f6a3b68b2219a
Raymond Groesbeck <45107361+rgroesbeck09@users.noreply.github.com>
2026-06-03 22:16:23 -0400

    Added in Hovercraft Class file and created a folder to hold the game classes

---
+++ Program2/Assets/Scripts/Classes/Hovercraft.cs
@@ -0,0 +1,68 @@
+/*
+ *  Final Variables
+ */
+const float DEFAULT_SPEED = 10;
+const int   DEFAULT_WIDTH = 10;
+const float DEFAULT_TURN = 10;
+
+/*
+ *  This is an enum to signify what the car type is. 
+ */
+public enum craft_type
+{ 
+    average,
+    fast,
+    cornering
+}
+
+
+///
+/*   This is the class file for the hovercraft
+ * 
+ * 
+ * 
+ */
+///
+
+public class Hovercraft :
+{
+    // Hoverecraft Variables
+    craft_type type;
+    float speed;
+    int width;
+    float turn_speed;
+
+    // default constructor for Hovercar
+    public Hovercraft()
+    {
+        type = craft_type.average;
+        speed = DEFAULT_SPEED;
+        width = DEFAULT_WIDTH;
+        turn_speed = DEFAULT_TURN;
+    }
+
+    // custom constructor for Hovercar
+    public Hovercraft(craft_type in_type, float in_speed, int in_width, float in_turn_speed)
+    {
+        type = in_type;
+        speed = in_speed;
+        width = in_width;
+        turn_speed = in_turn_speed;
+    }
+
+    // TODO: Create a function to change the car type
+    //        things to remember when changing the car type
+    //        need to change speed, cornering as well as the type
+    public Change_Car_Type()
+    { 
+    
+    }
+
+
+    // TODO: Lazers
+    public laser_shot()
+    { 
+    
+    }
+}
+
