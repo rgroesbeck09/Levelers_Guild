2ce3058b0a8b405352708526e26515aafc09ca9a
Raymond Groesbeck <45107361+rgroesbeck09@users.noreply.github.com>
2026-06-14 08:35:10 -0400

    Update the way the hovercraft drives and floats over the enviroment

--- Program2/Assets/Scripts/Movement.cs
+++ Program2/Assets/Scripts/Movement.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    // Public Variables
+    public float rayDistance = 10f;
+    public float rotationSpeed = 10f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +24,68 @@ public class Movement : MonoBehaviour
 
         Vector3 position = transform.position;
         
-        // set the game object's translation (not an increment)
-        transform.position = position;
+        // get the active terrain values
+        float terrainH = terrain.SampleHeight(position);
 
-        // translate by 0.1m on Z axis each frame for as long as
-        // the space bar is held down
-        if (Input.GetKey (KeyCode.Space))
-            // increment the game object's translation
-            transform.Translate(0, 0, 0.1f);
+        // set the game object's translation (not an increment)
+        transform.position = new Vector3(position.x, terrainH + 10, position.z);
+ 
+        if (Input.GetKey (KeyCode.W) ||
+            Input.GetKey (KeyCode.S) ||
+            Input.GetKey (KeyCode.A) ||
+            Input.GetKey (KeyCode.D))
+        {
+            // Set the allignment with the terrain
+            AlignWithTerrain();
+
+            // Forward and back
+            if (Input.GetKey (KeyCode.W))
+            {
+                // increment the game object's translation
+                transform.Translate(0, 0, 0.1f);
+            }
+            else if(Input.GetKey (KeyCode.S))
+            {
+                transform.Translate(0, 0, -0.1f);
+            }
+
+            // Rotational
+            if (Input.GetKey (KeyCode.A))
+            {
+                // increment the game object's translation
+                transform.Rotate(0, -0.1f, 0);
+            }
+            else if(Input.GetKey (KeyCode.D))
+            {
+                transform.Rotate(0, 0.1f, 0);
+            }
+        }
+    }
+
+    // this function is used to allign the object with the terrain
+    void AlignWithTerrain()
+    {
+        // setup the rays
+        Ray ray = new Ray(transform.position + Vector3.up, Vector3.down);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, rayDistance))
+        {
+            Vector3 terrainNormal = hit.normal;
+
+            // This is used to keep direction of object
+            Vector3 fSlope = Vector3.ProjectOnPlane(transform.forward, terrainNormal).normalized;
+
+            if (fSlope.sqrMagnitude > 0.001f)
+            {
+                Quaternion tRotation = Quaternion.LookRotation(fSlope, terrainNormal);
+
+                transform.rotation = Quaternion.Slerp(
+                    transform.rotation,
+                    tRotation,
+                    rotationSpeed * Time.deltaTime
+                );
+            }
+        }
     }
 }
